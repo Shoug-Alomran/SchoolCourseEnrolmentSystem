334f093a705550b841367d534d604ce49e96305f
Shoug-Alomran <shoug.alomran@hotmail.com>
2025-03-27 19:27:07 +0300

    Updated the updateCourseInfo method to remove the loop issue, fixed spelling mistake, and rearanged a few else block statments.

--- src/schoolcourseenrolmentsystem/Instructor.java
+++ src/schoolcourseenrolmentsystem/Instructor.java
@@ -65,45 +65,44 @@ public class Instructor extends User {
 
     }
 
-    public void updateCourseInfo(String courseCode, List<Course> courses, String newShedule, String newDescription,
+    public void updateCourseInfo(String courseCode, List<Course> courses, String newSchedule, String newDescription,
             List<Instructor> instructors, String newPassword, String newEmail, String newPhoneNumber,
             String newAddress) {
         // We must make sure that the course actually even exists.
-        boolean courseFound = false;
+        Course courseFound = null;
         // This following block is created for when the course actually exists.
         for (Course c : courses) {
             if (c.getCourseCode().equals(courseCode)) {
-                courseFound = true;
-                c.setSchedule(newShedule);
-                c.setDescription(newDescription);
-
-                // We must validate if the instructor is even assigned to the course they want
-                // to update
-                Instructor assignedInstructor = c.getInstructor();
-
-                if (assignedInstructor != null && assignedInstructor.getId().equals(this.getId())) {
-                    assignedInstructor.setPassword(newPassword);
-                    assignedInstructor.setEmail(newEmail);
-                    assignedInstructor.setPhoneNumber(newPhoneNumber);
-                    assignedInstructor.setAddress(newAddress);
-                    System.out.println("Instructor " + assignedInstructor.getName() + "'s"
-                            + " course information updated for " + c.getCourseName());
-                } // This is just in case if the course is assigned to a different instructor.
-                  // Its not empty but it also doesnt match the targetId.
-                else if (assignedInstructor != null) {
-                    System.out
-                            .println(assignedInstructor.getName() + " with the ID number: " + assignedInstructor.getId()
-                                    + " is not assigned to " + c.getCourseName() + ".");
-                } // You don't see it but its basically (assignedInstructor == null);
-                else {
-                    System.out.println("No instructor assigned to " + c.getCourseName() + ".");
-                }
-                return; // If there is a possiblity we found the course early we don't want to wait for
-                        // it to complete its round.
+                courseFound = c;
+                break;
             }
-        } // This following block is created for when the course doesn't actually exist.
-          // The ! means false
-        if (!courseFound) {
+        }
+        if (courseFound != null) {
+            courseFound.setSchedule(newSchedule);
+            courseFound.setDescription(newDescription);
+
+            // We must validate if the instructor is even assigned to the course they want
+            // to update
+            Instructor assignedInstructor = courseFound.getInstructor();
+
+            if (assignedInstructor != null && assignedInstructor.getId().equals(this.getId())) {
+                assignedInstructor.setPassword(newPassword);
+                assignedInstructor.setEmail(newEmail);
+                assignedInstructor.setPhoneNumber(newPhoneNumber);
+                assignedInstructor.setAddress(newAddress);
+                System.out.println("Instructor " + assignedInstructor.getName() + "'s"
+                        + " course information updated for " + courseFound.getCourseName());
+            } // This is just in case if the course is assigned to a different instructor.
+              // Its not empty but it also doesnt match the targetId.
+            else if (assignedInstructor != null) {
+                System.out
+                        .println(assignedInstructor.getName() + " with the ID number: " + assignedInstructor.getId()
+                                + " is not assigned to " + courseFound.getCourseName() + ".");
+            } // You don't see it but its basically (assignedInstructor == null);
+            else {
+                System.out.println("No instructor assigned to " + courseFound.getCourseName() + ".");
+            }
+        } else {
             System.out.println("Course with code " + courseCode + " not found.");
         }
     }
