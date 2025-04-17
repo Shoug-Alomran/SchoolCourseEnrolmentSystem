e4e0362c9788ee9be0a82216fe4c8f651f2f7ccf
Shoug-Alomran <shoug.alomran@hotmail.com>
2025-04-17 15:42:12 +0300

    Removed all setter logic because I created custom methods to replace them.

--- src/schoolcourseenrolmentsystem/User.java
+++ src/schoolcourseenrolmentsystem/User.java
@@ -11,13 +11,16 @@ public abstract class User<T> {
         INSTRUCTOR,
         ADMIN;
     }
-    private String name, id, password, email, phoneNumber;
+
+    private String name, ID, password, email, phoneNumber;
     private Role role;
     private String address;
 
-    public User() {}
+    public User() {
+    }
+
     // Constructor
-    public User(String name, String id, String password, String email, String phoneNumber, Role role, String address){
+    public User(String name, String id, String password, String email, String phoneNumber, Role role, String address) {
         setName(name);
         setId(id);
         setPassword(password);
@@ -32,23 +35,16 @@ public abstract class User<T> {
     }
 
     public void setName(String name) {
-        if (name == null || name.isEmpty()) {
-            System.out.println("Name cannot be null or empty.");
-            return;
-        } 
-            this.name = name;
+        this.name = name;
     }
 
     public String getId() {
-        return id;
+        return ID;
     }
 
     public void setId(String id) {
-        if (id == null || id.isEmpty()) {
-            System.out.println("ID cannot be null or empty.");
-            return;
-        } 
-            this.id = id;
+        // we do validation in prior to calling this method to avoid bloating it
+        this.ID = id;
     }
 
     public String getPassword() {
@@ -56,10 +52,6 @@ public abstract class User<T> {
     }
 
     public void setPassword(String password) {
-        if (password == null || password.isEmpty()) {
-            System.out.println("Password cannot be null or empty.");
-            return;
-        }
         this.password = password;
     }
 
@@ -68,11 +60,7 @@ public abstract class User<T> {
     }
 
     public void setEmail(String email) {
-        if (email == null || email.isEmpty()) {
-            System.out.println("Email cannot be null or empty.");
-            return;
-        } 
-            this.email = email;
+        this.email = email;
     }
 
     public String getPhoneNumber() {
@@ -80,47 +68,29 @@ public abstract class User<T> {
     }
 
     public void setPhoneNumber(String phoneNumber) {
-       // research on how to ensure that phone numbers follow the KSA pattern
-        if (phoneNumber.equals("temp")) {
-        this.phoneNumber = phoneNumber;
-        return;
-        }
-        if (phoneNumber.length() != 10) {
-        System.out.println("Length must be equal 10.");
-        return;
-        
-        }
+        // research on how to ensure that phone numbers follow the KSA pattern
         this.phoneNumber = phoneNumber;
     }
 
-   
-   
     public String getAddress() {
         return address;
     }
 
     public void setAddress(String address) {
-        if (address == null || address.isEmpty()) {
-        System.out.println("Address can't be null or empty.");
-        return;
-        }
         this.address = address;
     }
 
-    // Methods
-    public abstract T login(List<T> list, String id, String password);
-
-    public abstract String logout(T user);
-
+    // Custom Methods
     public Role getRole() {
         return role;
     }
 
     public void setRole(Role role) {
-        if (role == null) {
-            System.out.println("Role can't be null.");
-            return;
-        }
         this.role = role;
     }
+
+    // Abstract Methods
+    public abstract T login(List<T> list, String id, String password);
+
+    public abstract String logout(T user);
 }
