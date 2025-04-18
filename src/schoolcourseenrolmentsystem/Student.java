ae648ca4c2ed1aa810adf24b170ba6b581b2af0d
Shoug-Alomran <shoug.alomran@hotmail.com>
2025-04-18 19:05:36 +0300

    Deleted equals and hashCode methods. Added to string method and \n where its needed.

--- src/schoolcourseenrolmentsystem/Student.java
+++ src/schoolcourseenrolmentsystem/Student.java
@@ -1,4 +1,3 @@
-
 package schoolcourseenrolmentsystem;
 
 import java.util.*;
@@ -9,10 +8,12 @@ public class Student extends User<Student> {
     private List<Course> enrolledCourses;
 
     public Student() {
-        
+
     }
+
     // Constructor
-    public Student(String name, String id, String password, String email, String phoneNumber, String address, int creditLimit, List<Course> enrolledCourses) {
+    public Student(String name, String id, String password, String email, String phoneNumber, String address,
+            int creditLimit, List<Course> enrolledCourses) {
         super(name, id, password, email, phoneNumber, Role.STUDENT, address);
         setCreditLimit(creditLimit);
         // If the list is null, create a new one. Fixes NullPointerException.
@@ -55,21 +56,6 @@ public class Student extends User<Student> {
         return ("Student " + student.getName() + " logged out.");
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null || getClass() != obj.getClass())
-            return false;
-        Student other = (Student) obj;
-        return this.getId() != null && this.getId().equals(other.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-
     // Count the hours of the courses already enrolled
     public int totalCreditLimit() {
         int total = 0;
@@ -83,21 +69,21 @@ public class Student extends User<Student> {
     public void enroll_In_Course(Course c) {
         // 1. make sure credit limit is not excited and that the capacity is avaliable
         if (enrolledCourses.contains(c)) {
-            System.out.println("Already enrolled in (" + c.getCourseName() + ") (" + c.getCourseCode() + ").");
+            System.out.println("\nAlready enrolled in (" + c.getCourseName() + ") (" + c.getCourseCode() + ").");
         } else if (c.isFull()) {
             System.out.println("Cannot enroll in " + c.getCourseName() + " — course is full.");
         } // Check if the enrollment status is open or not
         else if (c.getEnrollmentStatus() == Course.EnrollmentStatusEnum.Closed) {
-            System.out.println("Enrollment status is closed.");
+            System.out.println("\nEnrollment status is closed.");
         } // Previously enrolled hours + new hours
         else if (totalCreditLimit() + c.getCreditHours() > creditLimit) {
-            System.out.println("Cannot enroll in " + c.getCourseName() + " — credit limit exceeded.");
+            System.out.println("\nCannot enroll in " + c.getCourseName() + " — credit limit exceeded.");
         } else {
             /// Adding the current student to the course's list of enrolled students.
             c.getEnrolledStudents().add(this);
             /// Adding the course to the student's list of enrolled courses.
             this.enrolledCourses.add(c);
-            System.out.println(getName() + " enrolled successfully");
+            System.out.println("\n" + getName() + " enrolled successfully");
         }
     }
 
@@ -116,7 +102,7 @@ public class Student extends User<Student> {
 
     public void viewCreditLimit(List<Student> students) {
         for (Student s : students) {
-            System.out.println(s.getName() + "'s credit limit is: " + s.getCreditLimit());
+            System.out.println("Your credit limit is: " + s.getCreditLimit());
 
             // (condition) ? (value if true) : (value if false);
             int enrolledHours = (s.getEnrolledCourses() != null) ? s.enrolledInHours() : 0;
@@ -135,7 +121,6 @@ public class Student extends User<Student> {
     }
 
     public void viewEnrolledCourses() {
-        // In the main I can write Student1.viewEnrolledCourses()
         if (enrolledCourses.isEmpty()) {
             System.out.println(getName() + " is not enrolled in any courses.");
         } else {
@@ -143,7 +128,7 @@ public class Student extends User<Student> {
             int index = 1;
             for (Course c : enrolledCourses) {
                 System.out.println(
-                        index + "- (" + c.getCourseName() + ") (" + c.getCourseCode() + ") ( " + c.getCreditHours()
+                        index + "- (" + c.getCourseName() + ") (" + c.getCourseCode() + ") (" + c.getCreditHours()
                                 + "). The schedule is: "
                                 + c.getSchedule() + ".");
                 index++;
@@ -157,13 +142,13 @@ public class Student extends User<Student> {
 
     public void viewAvailableCourses(List<Course> courses) {
         boolean avaliableCourses = false;
-        System.out.println("Available courses are: ");
+        System.out.println("\nAvailable courses are: ");
         for (Course c : courses) {
             if (c.isFull() == false && c.getEnrollmentStatus() == Course.EnrollmentStatusEnum.Open) {
                 avaliableCourses = true;
                 System.out.println("- (Course name: " + c.getCourseName() + ") (Course code: " + c.getCourseCode()
                         + ") (Course schedule: " + c.getSchedule() + ") (Course instructor: " + c.getInstructor()
-                        + ") (Course capacity: " + c.getCapacity() + ").");
+                        + ") (Course capacity: " + c.getCapacity() + ").\n");
             }
         }
         if (!avaliableCourses) {
@@ -172,13 +157,16 @@ public class Student extends User<Student> {
     }
 
     public void updateStudentPersonalInfo(String newPassword, String newEmail,
-            String newPhoneNumber,
-            String newAddress) {
-
+            String newPhoneNumber, String newAddress) {
         setPassword(newPassword);
         setEmail(newEmail);
         setPhoneNumber(newPhoneNumber);
         setAddress(newAddress);
-        System.out.println("Student " + getName() + "'s information updated.");
+        System.out.println("\nStudent " + getName() + "'s information updated.");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student data:\nID: %s\nName: %s", this.getId(), this.getName());
     }
 }
